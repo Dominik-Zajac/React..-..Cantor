f54b877984431c118ec3f89caf5dffe08c86a4dd
Wyrafinowany <zajac_dominik@onet.pl>
2019-07-18 10:20:21 +0200

    Create Header, Currencies and Wallet

--- src/containers/cantor/Cantor.js
+++ src/containers/cantor/Cantor.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import Header from '../../components/Header/Header';
+
 class Cantor extends Component {
     state = {}
     render() {
         return (
-            <h1>Cantor</h1>
+            <div>
+                <Header />
+            </div>
         );
     }
 }
