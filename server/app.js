cd514db6a38ed3af736695f89b48957ba9a21b40
Wyrafinowany <zajac_dominik@onet.pl>
2019-07-20 16:58:38 +0200

    Install mongoose and connected to database

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://dominik:pvn4gj@ds351807.mlab.com:51807/cantor');
+mongoose.connection.once('open', () => {
+    console.log('Connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,32 +1,40 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+    GraphQLID,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLString,
+    GraphQLObjectType,
+} = graphql;
 
 var users = [
     {
         id: '1',
         name: 'Dominik',
-        valueUSD: '10',
-        valueEUR: '20',
-        valueCHF: '100',
-        valueRUB: '50',
-        valueCZK: '0',
-        valueGBP: '60'
+        valueUSD: 10,
+        valueEUR: 20,
+        valueCHF: 100,
+        valueRUB: 50,
+        valueCZK: 0,
+        valueGBP: 60,
+        amountMoney: 1500,
     },
 ];
 
 const UserWallet = new GraphQLObjectType({
     name: 'Wallet',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
-        valueUSD: { type: GraphQLString },
-        valueEUR: { type: GraphQLString },
-        valueCHF: { type: GraphQLString },
-        valueRUB: { type: GraphQLString },
-        valueCZK: { type: GraphQLString },
-        valueGBP: { type: GraphQLString }
+        valueUSD: { type: GraphQLInt },
+        valueEUR: { type: GraphQLInt },
+        valueCHF: { type: GraphQLInt },
+        valueRUB: { type: GraphQLInt },
+        valueCZK: { type: GraphQLInt },
+        valueGBP: { type: GraphQLInt },
+        amountMoney: { type: GraphQLInt }
     })
 });
 
@@ -35,7 +43,7 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         wallet: {
             type: UserWallet,
-            args: { id: { type: GraphQLString } },
+            args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 return _.find(users, { id: args.id });
             }
